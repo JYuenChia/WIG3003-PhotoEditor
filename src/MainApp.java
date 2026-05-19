22d6d115bf694bfa4aa9839f3621f59b2661f820
Nicotmx <mayxin05@gmail.com>
2026-05-19 20:04:23 +0800

    Fix User Profile Orientaion and Light mode dark mode function

---
+++ src/MainApp.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Hello, JavaFX!");
+        Scene scene = new Scene(new StackPane(label), 400, 300);
+        stage.setTitle("JavaFX App");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
